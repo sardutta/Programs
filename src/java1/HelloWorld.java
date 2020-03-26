658ef182b4a328b8358bc4b16900b79e887c539c
My Pc <My Pc@alexz>
2020-03-26 21:18:40 +0530

    example change

---
+++ src/java1/HelloWorld.java
@@ -0,0 +1,19 @@
+package java1;
+import java.util.Scanner;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter two numbers");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		if(a>b)
+		{
+		System.out.println( a +" is greater than "+ b);
+		}
+		else
+			System.out.println( b +" is greater than "+ a);
+	}
+
+}
